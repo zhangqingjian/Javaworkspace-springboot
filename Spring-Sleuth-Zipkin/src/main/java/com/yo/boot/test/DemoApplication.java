c8467c413d6febdd79f349701a236f2f9b633ec0
zhangqingjian <mingrui-521@163.com>
2017-07-10 11:30:48 +0800

    v1

---
+++ Spring-Sleuth-Zipkin/src/main/java/com/yo/boot/test/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.yo.boot.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+public class DemoApplication
+{
+	@Bean
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
